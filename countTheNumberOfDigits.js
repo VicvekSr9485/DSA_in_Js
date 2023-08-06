e33b091339e9c8e5d050b1f734909105533ba5a4
VicvekSr <Olamideoso65@gmail.com>
2023-08-06 06:41:33 +0000

    add count the number of digits file

---
+++ countTheNumberOfDigits.js
@@ -0,0 +1,11 @@
+const countDigits = (num) => {
+    num = Math.abs(num);
+    let count = 0;
+    do {
+        count++;
+        num = Math.floor(num / 10);
+    } while (num > 0);
+    return count;
+}
+
+console.log(countDigits(-23567));
