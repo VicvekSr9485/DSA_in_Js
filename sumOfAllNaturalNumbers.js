f5ab7be7e2eae917d5948cdc1a3a0733c0fb1036
VicvekSr <Olamideoso65@gmail.com>
2023-08-06 06:29:13 +0000

    add sum of all natural numbers from 1 to n

---
+++ sumOfAllNaturalNumbers.js
@@ -0,0 +1,12 @@
+const sumOfAll = (num) => {
+    /*
+    let sum  = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+    */
+    return num*(num+1)/2;
+}
+console.log(sumOfAll(5));
+
