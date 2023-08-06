319543b0ef51273115304e0187eaaa9f34c3bcfa
VicvekSr <Olamideoso65@gmail.com>
2023-08-06 06:34:45 +0000

    add sum of digits of a number

--- sumOfDigits.js
+++ sumOfDigits.js
@@ -1,3 +1,9 @@
 const sumOfDigits = (num) => {
-    
+    let sum = 0;
+    while (num > 0) {
+        sum += num%10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
 }
+console.log(sumOfDigits(12345))
