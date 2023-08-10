526c54bfc3953e94eefab08d06633402f129111b
VicvekSr <Olamideoso65@gmail.com>
2023-08-10 02:34:08 +0000

    add missing number algo

---
+++ missingNumber.js
@@ -0,0 +1,10 @@
+const missingNumber = (num) => {
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += num[i];
+    }
+    return num.length*(num.length + 1)/2 - sum;
+    //using reduce array method
+    // return num.length*(num.length + 1)/2 - num.reduce((acc, num) => acc + num);
+}
+console.log(missingNumber([1,4,3]))
